6d411d07cc61b727c3b803fe27304d7763e76b0f
MonsieurLuge <monsieurluge@gmail.com>
2016-08-24 17:31:25 +0200

    ugly (and temporary) background

---
+++ src/js/background.js
@@ -0,0 +1,60 @@
+/**
+ * Background object
+ * @param {Canvas} canvas
+ */
+function Background(canvas) {
+    this.__cache            = false;
+    this.__canvas           = canvas;
+    this.__plainColor       = '#dbdbcf';
+    this.__transparentColor = 'rgba(0, 0, 0, 0.02)';
+}
+
+/**
+ * Clears the background
+ */
+Background.prototype.clear = function() {
+    this.__cache = false;
+}
+
+/**
+ * Draws the background
+ * @param {Canvas} canvas
+ */
+Background.prototype.draw = function(canvas) {
+    if (this.__cache == true) {
+        canvas.context2d().drawImage(
+            this.__canvas.canvas(),
+            0,
+            0,
+            canvas.width(),
+            canvas.height()
+        );
+
+        return;
+    }
+
+    // simple color
+    this.__canvas.context2d().fillStyle = this.__plainColor;
+    this.__canvas.context2d().beginPath();
+    this.__canvas.context2d().rect(0, 0, this.__canvas.width(), this.__canvas.height());
+    this.__canvas.context2d().fill();
+
+    // random squares
+    this.__canvas.context2d().fillStyle = this.__transparentColor;
+
+    for (var index = 0; index < 100; index++) {
+        this.__canvas.context2d().beginPath();
+        this.__canvas.context2d().rect(
+            Math.floor(Math.random() * this.__canvas.width()) - 20,
+            Math.floor(Math.random() * this.__canvas.height()) - 20,
+            Math.floor(Math.random() * this.__canvas.width() / 2),
+            Math.floor(Math.random() * this.__canvas.height() / 2)
+        );
+        this.__canvas.context2d().fill();
+    }
+
+    // cache the canvas
+    this.__cache = true;
+
+    this.draw(canvas);
+}
