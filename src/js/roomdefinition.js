c2e5fb068065667d1975479f275fb5b761d692c3
monsieurluge <monsieurluge@gmail.com>
2016-09-03 20:00:32 +0200

    room definition basic methods

--- src/js/roomdefinition.js
+++ src/js/roomdefinition.js
@@ -1,9 +1,45 @@
 /**
  * RoomDefinition object
+ * @param {string} name
+ * @param {string} type
  * @param {Biome} biome
- * @param {Entity[]} mandatoryEntities
+ * @param {Entities} mandatoryEntities
  */
-function RoomDefinition(biome, mandatoryEntities) {
+function RoomDefinition(name, type, biome, mandatoryEntities) {
     this->__biome             = biome;
     this->__mandatoryEntities = mandatoryEntities;
+    this->__name              = name;
+    this->__type              = type;
+}
+
+/**
+ * Returns the Biome
+ * @return {Biome}
+ */
+RoomDefinition.prototype.biome = function() {
+    return this->__biome;
+}
+
+/**
+ * Returns the mandatory entities
+ * @return {Entities}
+ */
+RoomDefinition.prototype.mandatoryEntities = function() {
+    return this->__mandatoryEntities;
+}
+
+/**
+ * Returns the name
+ * @return {string}
+ */
+RoomDefinition.prototype.name = function() {
+    return this->__name;
+}
+
+/**
+ * Returns the type
+ * @return {string}
+ */
+RoomDefinition.prototype.type = function() {
+    return this->__type;
 }
