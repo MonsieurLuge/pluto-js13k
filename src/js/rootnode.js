1ac9216b338116de0c722a57b945ce843f1f3f5b
Laurent Vergé <laurent.verge-prestataire@laposte.fr>
2016-09-09 13:15:00 +0200

    add RootNode

---
+++ src/js/rootnode.js
@@ -0,0 +1,40 @@
+/**
+ * RootNode object (RootNode is a Node)
+ * @param {string} name
+ */
+function RootNode(name) {
+    this.__childrens = [];
+    this.__name      = name;
+}
+
+/**
+ * Adds a children
+ * @param {string} name
+ */
+RootNode.prototype.addChildren = function(name) {
+    this.__childrens.push(name);
+}
+
+/**
+ * Returns the node's childrens
+ * @return {array}
+ */
+RootNode.prototype.childrens = function() {
+    return this.__childrens;
+}
+
+/**
+ * Returns the name
+ * @return {string}
+ */
+RootNode.prototype.name = function() {
+    return this.__name;
+}
+
+/**
+ * There is no parent for the root node, so throw an exception
+ * @throws {string}
+ */
+RootNode.prototype.parent = function() {
+    throw 'no parent for the node "' + this.__name + '" -> it\'s the root one';
+}
